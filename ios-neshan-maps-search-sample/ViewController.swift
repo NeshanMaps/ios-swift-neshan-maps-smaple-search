8cf389e5cc07978d32e927c5d07bb8969a0f1733
M.Madadi <M.Madadi@apple-nic-3.local>
2018-12-23 08:55:41 +0330

    Initial Commit

---
+++ ios-neshan-maps-search-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-neshan-maps-search-sample
+//
+//  Created by M.Madadi on 12/23/18.
+//  Copyright © 2018 Rajman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
